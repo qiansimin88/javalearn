4315e5dbcd5a02a06fe9b542fe9282796e02570f
qsm <348867341@qq.com>
2017-10-25 22:06:47 +0800

    fix

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by qsm on 2017/10/23.
+ */
+public class HelloWorld {
+    public static void main (String[] args) {
+        //字符串变量
+        String who = "helloWorld!!!!😮";
+        System.out.println( who );
+    }
+}
