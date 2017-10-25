0ca20d233a575febce15b7673abee2df707542f0
qsm <348867341@qq.com>
2017-10-25 22:17:13 +0800

    fix

---
+++ src/var_0.java
@@ -0,0 +1,13 @@
+/**
+ * Created by qsm on 2017/10/25.
+ */
+/*
+*   变量定义和使用的注意事项
+*   1.变量定义后，不复制，不能使用
+* */
+public class var_0 {
+    public static void main (String[] args) {
+        int i = 1000;
+        System.out.println( i );
+    }
+}
